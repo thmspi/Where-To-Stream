fddb44ccb505973889173ca882e9daf70b00d572
thomas <amiscremail@gmail.com>
2025-09-01 19:08:40 +0200

    updated main.tf to use zip

--- infra/main.tf
+++ infra/main.tf
@@ -164,8 +164,8 @@ resource "aws_lambda_function" "watch" {
   role             = aws_iam_role.lambda.arn
   runtime          = "nodejs18.x"
   handler          = "watch.handler"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = data.archive_file.lambda_watch_zip.output_path
+  source_code_hash = data.archive_file.lambda_watch_zip.output_base64sha256
 
   environment {
     variables = merge(var.lambda_env, {
@@ -184,8 +184,8 @@ resource "aws_lambda_function" "search" {
   role             = aws_iam_role.lambda.arn
   runtime          = "nodejs18.x"
   handler          = "search.handler"
-  filename         = data.archive_file.search_zip.output_path
-  source_code_hash = data.archive_file.search_zip.output_base64sha256
+  filename         = data.archive_file.lambda_search_zip.output_path
+  source_code_hash = data.archive_file.lambda_search_zip.output_base64sha256
   environment {
     variables = merge(var.lambda_env, {
       TMDB_KEY = var.tmdb_key
